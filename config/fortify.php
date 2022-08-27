1f51837f52d0997872f394f0b8dd16cf127c0f18
vassilidev <vassilidevnet@gmail.com>
2022-08-27 14:31:26 +0200

    Config Laravel Fortify ⚙️

--- config/fortify.php
+++ config/fortify.php
@@ -132,16 +132,8 @@ return [
     */
 
     'features' => [
-        Features::registration(),
         Features::resetPasswords(),
-        // Features::emailVerification(),
-        Features::updateProfileInformation(),
         Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            // 'window' => 0,
-        ]),
     ],
 
 ];
